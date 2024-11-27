804a6850c7645b34cfb7ed2727369f2e7e022258
TheNoobSpine <davidespinhamarques@gmail.com>
2024-11-27 22:49:07 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+struct Rabbit
+{
+    int gen;
+    int x;
+    int y;
+};
+
+struct Fox
+{
+    int gen;
+    int x;
+    int y;
+    int food;
+};
+
+struct Rock
+{
+    int x;
+    int y;
+};
+
+void print_gen(struct Rabbit *rabbits, struct Fox *foxes, struct Rock *rocks, int size){
+
+}
+
+void read_input(){
+
+}
+
+void print_output(){
+    
+}
+
+
+int main(){
+
+}
